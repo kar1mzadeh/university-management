fd05355044fb7da7a7620244e39ecbabd6af3140
unknown <rkarimov17969@ada.edu.az>
2025-02-27 01:36:06 +0400

    Testing

--- model/Research.java
+++ model/Research.java
@@ -2,9 +2,12 @@ package model;
 
 import java.util.ArrayList;
 
+import model.person.Faculty;
+
 public class Research {
     private String title;
     private String journal;
     private String paperAbstract;
-    private ArrayList<String> authors;
+    private ArrayList<Faculty> authors;
+    
 }
